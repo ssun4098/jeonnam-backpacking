4d0fe7fee5ebdf728f52108341b82b9dfc0d3637
Administrator <admin@example.com>
2023-07-22 20:41:26 +0900

    Create Project

---
+++ src/main/java/daonplace/com/jeonnambackpacking/JeonnamBackpackingApplication.java
@@ -0,0 +1,13 @@
+package daonplace.com.jeonnambackpacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonnamBackpackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeonnamBackpackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daonplace/com/jeonnambackpacking/JeonnamBackpackingApplicationTests.java
@@ -0,0 +1,13 @@
+package daonplace.com.jeonnambackpacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeonnamBackpackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
